6f7b9985e609da3bab5227e3778e715964b89ea8
axetroy <troy450409405@gmail.com>
2017-04-26 11:04:33 +0800

    update

--- index.js
+++ index.js
@@ -2,11 +2,45 @@
  * Created by axetroy on 17-4-26.
  */
 const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 app.get('/', function(req, res) {
-  res.send('Hello World, Here are Heroku');
+  res.send('Hello World.');
+});
+
+app.get('/oauth', function(req, res) {
+  const code = req.query.code;
+  axios
+    .post(
+      'https://github.com/login/oauth/access_token',
+      {
+        code,
+        client_id: 'b8257841dd7ca5eef2aa',
+        client_secret: '4da33dd6fcb0a01d395945ad18613ecf9c12079e'
+      },
+      {
+        responseType: 'json',
+        headers: {
+          Accept: 'application/json'
+        }
+      }
+    )
+    .then(function(response) {
+      res.status(200).json(response.data);
+    })
+    .catch(function(err) {
+      res.status(400).json({ err: err + '' });
+    });
 });
 
 app.listen(PORT, function() {
