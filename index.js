aa7abc001fe79ffc51213622e4a4dfe3f0ed2c32
axetroy <troy450409405@gmail.com>
2017-04-26 11:10:23 +0800

    update

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
+
 app.get('/', function(req, res) {
   res.send('Hello World.');
 });
